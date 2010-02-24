31386b89336537150bd4dea8b724170f64ac83b3
arfrank <arfrank@gmail.com>
2010-02-24 18:10:16 -0500

    Added some text for layout placement

--- system/application/views/base/header.php
+++ system/application/views/base/header.php
@@ -1,5 +1,9 @@
 <div id="all-header">
-	<div class="span-24 clear" id="barline"></div>
+	<div class="span-24 clear" id="barline">
+		<div class="center">
+			This is the area for tiny gray text. All tiny gray text goes here.  What else would go here besides tiny gray text? If you think other things should go here, let me know.
+		</div>
+	</div>
 	<div class="span-24" id="header">
 		<a href="/"><div class="span-3 prepend-1" id="logo"><img class="span-3" alt="logo" title="logo" /></div></a>
 		<div class="span-8 prepend-2" id="title">YouTastic.com</div>
