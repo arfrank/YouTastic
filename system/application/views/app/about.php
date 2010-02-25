ed609dfa7b22b4be3f904ff11227f2905a2c5b1a
arfrank <arfrank@gmail.com>
2010-02-25 00:42:50 -0500

    Added github link

--- system/application/views/app/about.php
+++ system/application/views/app/about.php
@@ -1,5 +1,6 @@
 <div class="span-22 prepend-2">
 	<div class="large">About Everything Page</div>
 	<div class="span-22"><div class="span-4 center"><img alt="picture of aaron" ><br /><a href="http://www.arfrank.com">Aaron Frank</a></div><div class="span-16">In this area we will include a made up bio.  This section will possible have multiple people included. In this area we will include a made up bio.  This section will possible have multiple people included. In this area we will include a made up bio.  This section will possible have multiple people included. In this area we will include a made up bio.  This section will possible have multiple people included.</div></div>
-	<div id="info" class="span-22">This project was concieved in the head of Aaron as he wanted to take pictures of the things he encountered in a daily basis and make a game out of it.</div>
+	<div id="info" class="span-22 clear">This project was concieved in the head of Aaron as he wanted to take pictures of the things he encountered in a daily basis and make a game out of it.<br /><br />
+	<div class="span-22"><a href="http://github.com/arfrank/YouTastic">GitHub YouTastic Code Base</a></div></div>
 </div>
