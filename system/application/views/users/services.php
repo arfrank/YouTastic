c47f119142af2a27fe62f56529b3e5a56f8f87bf
arfrank <arfrank@gmail.com>
2010-02-25 23:15:00 -0500

    Adding users templates

---
+++ system/application/views/users/services.php
@@ -0,0 +1,3 @@
+<div class="center span-22 prepend-1"><?php if($flashmessage) { echo $flashmessage; } ?></div>
+<div id="main-content" class="span-14 prepend-2 border"></div>
+<div class="span-6 append-1 prepend-1 last">Sidebar</div>
