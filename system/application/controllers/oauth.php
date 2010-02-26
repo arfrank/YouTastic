bf5a7be18679bcc8c84aff7a1ef9a1e5c493158b
arfrank <arfrank@gmail.com>
2010-02-25 23:13:12 -0500

    Adding libraries to harness off of to enable oauth and model manipulation. Also basic oauth stuff for twitter

---
+++ system/application/controllers/oauth.php
@@ -0,0 +1,45 @@
+<?php
+
+class Oauth extends Controller {
+
+	function Oauth()
+	{
+		parent::Controller();	
+		$this->load->model('Accounts');
+		
+	}
+
+	function index(){
+		redirect('/');
+	}
+	
+	//Adding twitter as a feed
+	function twitter(){
+		// Set app keys
+		$consumer_key = $this->config->item('twitter_key');
+		$consumer_key_secret = $this->config->item('twitter_secret');
+
+		$this->load->library('twitter');
+		//Make initial calls to grant access
+		$auth = $this->twitter->oauth($consumer_key, $consumer_key_secret, null, null);
+		if ( isset($auth['access_token']) && isset($auth['access_token_secret']) )
+		{
+			//Make sure service ID is correct for correct later processing
+			$this->load->model('services');
+			$id=$this->Services->find('service="twitter"','id');
+			$data['service_id']=$id['id'];
+			$data['user_id']=$this->session->userdata('user_id');
+				$data['data']=serialize(array('access_token'=>$auth['access_token'],'access_token_secret'=>$auth['access_token_secret']));
+			//load model to create a new account
+			$this->Accounts->insert($data); // SAVE THE ACCESS TOKENS
+			$this->session->set_flashdata('message','<div class="success">Congratulations, you have successfully added a Twitter account to your feed.</div>');
+			redirect('users/services');
+		}else{
+			$this->session->set_flashdata('message','<div class="error">Sorry, but we weren\'t able to add that Twitter account to your account, please try again.  If this problem persists please <a href="/contact">contact us.</a></div>');
+			redirect('users/services');
+		}
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./system/application/controllers/welcome.php */
